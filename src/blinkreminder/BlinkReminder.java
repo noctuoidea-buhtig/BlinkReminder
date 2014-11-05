bca4a2b419654cd07bdebfb09017ab814003bef1
Oliver Zurr <noctuoidea-buhtig@yahoo.com>
2014-11-05 21:11:49 +0100

    Enable/Disable

--- src/blinkreminder/BlinkReminder.java
+++ src/blinkreminder/BlinkReminder.java
@@ -11,10 +11,17 @@ public class BlinkReminder{
 	private SystemTray tray=null;
 	private static final String useTrayMessageLabel="Use Tray Message";
 	private final Preferences settings=Preferences.userNodeForPackage(getClass());
-	private final JRadioButtonMenuItem useTrayMessage=new JRadioButtonMenuItem(
+	private final JCheckBoxMenuItem useTrayMessage=new JCheckBoxMenuItem(
 		useTrayMessageLabel,
 		settings.getBoolean(useTrayMessageLabel, SystemTray.isSupported())
 	);
+	private final JCheckBoxMenuItem enabled=new JCheckBoxMenuItem("Enabled",true);
+	private TimerTask tempDisableTask=new TimerTask() {
+		@Override
+		public void run(){
+		}
+	};
+	final java.util.Timer tempDisableTimer=new java.util.Timer();
 
 	public BlinkReminder(){
 		final String name=getClass().getSimpleName();
@@ -37,6 +44,30 @@ public class BlinkReminder{
 					settings.putBoolean(useTrayMessageLabel, useTrayMessage.isSelected());
 				}
 			});
+			JMenuItem tempDisable = new JMenuItem("Disable for 15 min");
+			tempDisable.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e){
+					enabled.setSelected(false);
+					tempDisableTask.cancel();
+					tempDisableTask=new TimerTask() {
+						@Override
+						public void run(){
+							enabled.setSelected(true);
+						}
+					};
+					tempDisableTimer.schedule(
+						tempDisableTask,
+						15*60*1000
+					);
+				}
+			});
+			enabled.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e){
+					tempDisableTask.cancel();
+				}
+			});
 			JMenuItem about = new JMenuItem("About");
 			about.addActionListener(new ActionListener() {
 				@Override
@@ -44,9 +75,10 @@ public class BlinkReminder{
 					JOptionPane.showMessageDialog(
 						null,
 						name+" was written in Java by Oliver Zurr.\n"
-						+ "It is supposed to help you blink more often, because\n"
+						+ "It willt help you blink more often, even though\n"
 						+ "humans blink less when staring at their screen.\n"
-						+ "This is version 2. Available on GitHub.com .",
+						+ "This is version 4.\n"
+						+ "Available on GitHub.com and SourceForge.net",
 						"About "+name,
 						JOptionPane.INFORMATION_MESSAGE
 					);
@@ -59,8 +91,11 @@ public class BlinkReminder{
 					System.exit(0);
 				}
 			});
+			trayMenu.add(tempDisable);
 			trayMenu.add(useTrayMessage);
 			trayMenu.add(about);
+			trayMenu.add(new JSeparator());
+			trayMenu.add(enabled);
 			trayMenu.add(new JSeparator());
 			trayMenu.add(exit);
 			mainIcon.addMouseListener(
@@ -90,29 +125,31 @@ public class BlinkReminder{
 			new TimerTask() {
 				@Override
 				public void run() {
-					if(
-						mainIcon==null || // no systray, use shaped window instead
-						!useTrayMessage.isSelected()
-					){
-						ImageShapeWindow.spawnEye();
-					}else{
-						mainIcon.displayMessage("😉", "Blink!",
-						TrayIcon.MessageType.NONE);
-						if (System.getProperty("os.name").contains("Linux")){
-							(new java.util.Timer()).schedule( // Linux messages often stay
-								new TimerTask() {
-									@Override
-									public void run() {
-										tray.remove(mainIcon);
-										try{
-											tray.add(mainIcon);
-										}catch(AWTException ex){
-											ex.printStackTrace();
+					if(enabled.isSelected()){
+						if(
+							mainIcon==null || // no systray, use shaped window instead
+							!useTrayMessage.isSelected()
+						){
+							ImageShapeWindow.spawnEye();
+						}else{
+							mainIcon.displayMessage("😉", "Blink!",
+							TrayIcon.MessageType.NONE);
+							if (System.getProperty("os.name").contains("Linux")){
+								(new java.util.Timer()).schedule( // Linux messages often stay
+									new TimerTask() {
+										@Override
+										public void run() {
+											tray.remove(mainIcon);
+											try{
+												tray.add(mainIcon);
+											}catch(AWTException ex){
+												ex.printStackTrace();
+											}
 										}
-									}
-								},
-								3000
-							);
+									},
+									3000
+								);
+							}
 						}
 					}
 				}
